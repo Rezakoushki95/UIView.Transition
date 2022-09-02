fd2222a4ee17871a41632a0ada005e6b58e8ffa6
rezakoushki95 <rezaaarhus@hotmail.com>
2022-09-02 10:44:46 +0200

    3.8 ex.1 A single page app illustrating the use of UIView.transition combined with a conditional use of DispatchQueue

--- UIView.Transition/ViewController.swift
+++ UIView.Transition/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	@IBOutlet weak var emojiLabel: UILabel!
+	@IBOutlet weak var delaySwitch: UISwitch!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
+	
+	func addTransition(emoji: String) {
+		UIView.transition(
+				with: emojiLabel,
+				duration: 0.5,
+				options: .transitionFlipFromBottom) {
+					self.emojiLabel.text = emoji
+				}
+	}
 
-
+	@IBAction func ghostButtonPressed(_ sender: UIButton) {
+		if delaySwitch.isOn {
+			DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+				self.addTransition(emoji: "👻")
+			}
+		} else {
+			addTransition(emoji: "👻")
+		}
+	}
+	
+	@IBAction func pumpkinButtonPressed(_ sender: UIButton) {
+		if delaySwitch.isOn {
+			DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+				self.addTransition(emoji: "🎃")
+			}
+		} else {
+			addTransition(emoji: "🎃")
+		}
+	}
 }
 
